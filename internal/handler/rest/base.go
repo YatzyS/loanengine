93ce814a680c8096b162970de60ae16c9ac1ddab
Yash <yasket@gmail.com>
2025-02-06 20:21:50 +0530

    feat: Add routes and handler functions

--- internal/handler/rest/base.go
+++ internal/handler/rest/base.go
@@ -1,8 +1,41 @@
 package rest
 
-type RestHandler interface{}
+import "github.com/gin-gonic/gin"
+
+type RestHandler interface {
+	Propose(c *gin.Context)
+	Approve(c *gin.Context)
+	Invest(c *gin.Context)
+	Disburse(c *gin.Context)
+	GetState(c *gin.Context)
+	GetProposedLoans(c *gin.Context)
+}
 
 type restHandler struct{}
+
+func (r restHandler) Propose(c *gin.Context) {
+	panic("implement me")
+}
+
+func (r restHandler) Approve(c *gin.Context) {
+	panic("implement me")
+}
+
+func (r restHandler) Invest(c *gin.Context) {
+	panic("implement me")
+}
+
+func (r restHandler) Disburse(c *gin.Context) {
+	panic("implement me")
+}
+
+func (r restHandler) GetState(c *gin.Context) {
+	panic("implement me")
+}
+
+func (r restHandler) GetProposedLoans(c *gin.Context) {
+	panic("implement me")
+}
 
 func NewRestHandler() RestHandler {
 	return &restHandler{}
--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -7,5 +7,17 @@ import (
 
 func (a *App) SetupRoutesAndMiddleware(router *gin.RouterGroup, restHandler rest.RestHandler) {
 	router.Use(gin.Recovery())
-	// TODO: Setup routes here
+
+	v1 := router.Group("/v1")
+	loan := v1.Group("/loan")
+	loan.POST("/propose", restHandler.Propose)
+	loan.POST("/invest", restHandler.Invest)
+	loan.POST("/approve", restHandler.Approve)
+	loan.POST("/disburse", restHandler.Disburse)
+
+	agent := v1.Group("/agent")
+	agent.GET("/loans", restHandler.GetProposedLoans)
+
+	admin := v1.Group("/admin")
+	admin.GET("/loan/status", restHandler.GetState)
 }
